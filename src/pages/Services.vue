6b7b6e5d9644b830c52318e3b2d763c142365d14
Ecovirtual <ramiro.ecovirtual@gmail.com>
2019-04-09 09:51:22 +0200

    Add services Images

--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -2,7 +2,7 @@
   <main role="main" id="services">
 
     <valpo-youtube-splash class="splash" :image="images.giuliana" :video="video">
-      <h1>Events</h1>
+      <h1>Our Services</h1>
     </valpo-youtube-splash>
 
     <valpo-content class="services-content flex-rows align-center">
@@ -105,30 +105,29 @@
 </template>
 
 <script>
-import ValpoYoutubeSplash from "@/components/ValpoYoutubeSplash";
-import ValpoContent from "@/components/ValpoContent";
-import ValpoList from "@/components/ValpoList";
-import ValpoListItem from "@/components/ValpoListItem";
-import ValpoButton from "@/components/ValpoButton";
-import ValpoTwoColSection from "@/components/ValpoTwoColSection";
-import giuliana from "../assets/images/image-giuliana.jpg";
-import beach from "../assets/images/image-beach.jpg";
+import ValpoYoutubeSplash from "@/components/ValpoYoutubeSplash"
+import ValpoContent from "@/components/ValpoContent"
+import ValpoList from "@/components/ValpoList"
+import ValpoListItem from "@/components/ValpoListItem"
+import ValpoButton from "@/components/ValpoButton"
+import ValpoTwoColSection from "@/components/ValpoTwoColSection"
 
-import iconBuilding from "../assets/images/icon-building.svg";
-import iconDesign from "../assets/images/icon-design.svg";
-import iconVideo from "../assets/images/icon-video.svg";
-import iconMarketing from "../assets/images/icon-marketing.svg";
-import iconSocial from "../assets/images/icon-social.svg";
+// Images
+import giuliana from "../assets/images/image-giuliana.jpg"
+import beach from "../assets/images/image-beach.jpg"
+import ship from "../assets/images/services/ship_02.jpg"
+import tipi from "../assets/images/services/tipi.jpg"
+import gardensBabylon from "../assets/images/services/festival01.jpg"
+import vrijland from "../assets/images/services/festival02.jpg"
+import ceuvel from "../assets/images/services/festival03.jpg"
+import intoTheWoods from "../assets/images/services/festival05.jpg"
+import brestMaritime from "../assets/images/services/festival06.jpg"
+import conceptRentFriend from "../assets/images/services/concept_rent_friend.jpg"
+import conceptOrigamiCrane from "../assets/images/services/concept_cranes.jpg"
+import conceptGlowDark from "../assets/images/services/concept_glow_dark.jpg"
+import conceptDj from "../assets/images/services/concept_dj.jpg"
+import conceptMermaid from "../assets/images/services/concept_mermaid.jpg"
 
-import light from "../assets/images/projects/image-light.jpg";
-import qr from "../assets/images/projects/image-qr.jpg";
-import birds from "../assets/images/projects/image-birds.jpg";
-import boatbike from "../assets/images/projects/valparaiso_projects_boatbike.jpg";
-import festivals from "../assets/images/projects/valparaiso_projects_festivals.jpg";
-import glass from "../assets/images/projects/valparaiso_projects_glass.jpg";
-import grulla from "../assets/images/projects/valparaiso_projects_grulla.jpg";
-
-// TODO: add new pictures
 
 export default {
   name: "Volunteer",
@@ -141,102 +140,95 @@ export default {
     ValpoTwoColSection
   },
   data: () => ({
-    images: { giuliana, beach },
+    images: {
+      giuliana,
+      beach,
+      ship,
+      tipi,
+      gardensBabylon,
+      vrijland,
+      ceuvel,
+      intoTheWoods,
+      brestMaritime,
+      conceptRentFriend,
+      conceptOrigamiCrane,
+      conceptGlowDark,
+      conceptDj,
+      conceptMermaid
+    },
     video: {
       src: "cjC9ggS7EVE"
     },
-    tasks: [
-      {
-        icon: iconBuilding,
-        alt: "Building & Repairing"
-      },
-      {
-        icon: iconDesign,
-        alt: "Design"
-      },
-      {
-        icon: iconVideo,
-        alt: "Multimedia Production"
-      },
-      {
-        icon: iconMarketing,
-        alt: "Marketing"
-      },
-      {
-        icon: iconSocial,
-        alt: "Social Media"
-      }
-    ],
     projects: [
       {
-        image: light,
+        image: gardensBabylon,
         title: "Gardens of Babylon",
         description: ""
       },
       {
-        image: qr,
+        image: vrijland,
         title: "Vrijland",
         description: ""
       },
       {
-        image: boatbike,
+        image: ceuvel,
         title: "Ceuvel",
         description: ""
       },
       {
-        image: festivals,
+        image: intoTheWoods,
         title: "Into the woods",
         description: ""
       },
       {
-        image: glass,
+        image: brestMaritime,
         title: "Brest Maritime",
         description: ""
       }
     ],
     spaces: [
       {
-        image: light,
+        image: ship,
         title: "The Valparaiso",
         description: "Deck and doors are thrown open. Visitors are welcomed in and invited to create their own place in the history of this 100-year old boat. We are a floating world of magic, a Fata Morgana on the water. The top deck is a lounge draped with blankets and pillows, and the ships also hosts a DJ booth and small stage. This wildly-popular venue can be reached by small boat or by swimming. It is usually moored in Amsterdam, as the hub of our creative community, but with a crane it can be put on long-distance transport."
       },
       {
-        image: qr,
+        image: tipi,
         title: "Tipi Tent",
         description: "The tipi is an inspiring place to relax. This eight metre space can host up to forty-five people, and includes a DJ deck and small stage. It is home to a beautiful mobile of origami cranes, is lit by UV lights, and is perfect for both relaxing and partying. But the experience we create is about more than the lights and Persian carpets. We invite people to step inside and become part of this world of imagination we build together."
       }
     ],
     concepts: [
       {
-        image: light,
+        image: conceptRentFriend,
         title: "Rent a Friend",
         description: "Ever lost a friend at a festival? It happens to all of us. Now you don’t have to be alone - you can rent a friend! If you need help finding your lost friends, or your lost keys, or you just don’t want to go to a dance alone, we have lots of friends in stock that you can rent for free! They come equipped with an alarm clock, and return automatically when it goes off! Our visitors are also invited to put themselves up to rent. Festivals are amazing places to meet people, and what better way to make new friends?",
         collaborator: "",
         link: ""
       },
       {
-        image: qr,
+        image: conceptOrigamiCrane,
         title: "Origami Crane Installation",
         description: "Visitors to Suzanne’s art studio are often asked to paint a piece of paper, which she then transforms into origami cranes. Over the years this had grown into a substantial flock of unique, hand-painted birds that radiate with the various souls that have contributed to it. This year, we are asking visitors to join in, painting their own pieces of paper to become part of this art installation and centrepiece that represents the beautiful things we can achieve through connection and community.",
         collaborator: "Suzanne Dirne",
         link: "http://overratedenzo.nl/"
       },
       {
-        image: light,
+        image: conceptGlowDark,
         title: "Glow in the Dark Dating",
         description: "Ever tried dating in the dark? With our quizmaster deciding the rhythm of the night, all extra lights are off and all the glow in the dark makeup is on. Inspiring luminous connections in unique spaces under the romance of black lighting, it will make a great ‘how we met’ story in years to come. People will choose coloured bands to wear based on their dating preferences – purple for women, blue for men, and white for non-binary people. If they have no preferences, they can wear all three!",
         collaborator: "",
         link: ""
       },
       {
-        image: qr,
+        image: conceptDj,
         title: "Music by Kevin Valentine",
         description: "Our DJ stage is a platform for emerging artists and hidden gems. We select our DJs carefully and make sure the music fits the vibe of the festival and our space. Depending on the program the DJ will play down tempo music or music that it will make it impossible for you to stand still! The intimate atmosphere makes sure this secret stage will be something all visitors will remember. Our doors are always open to new opportunities, new connections, and new experiences. Kevin also invites other unfamiliar DJs connected to the genre to play, so for this contact him on Soundcloud.",
         collaborator: "Kevin Valentine",
         link: "http://www.soundcloud.com/kevinvalentine"
       },
       {
-        image: light,
+        image: conceptMermaid,
         title: "Mobile Mermaid",
         description: "Normal mermaids don’t do too well out of water. They just flop around and flap their fins, and you have to come to see them. But the mermaid that the pirates captured is on wheels! She is an oracle who listens to the whispers of the wind and the waves, knows the stories of the sky and the sea - and the secrets of legless modes of travel. She drives around the festival, going from stage to stage. Who better than a mythical mermaid to bring a sense of calm and normality to this crazy world?",
         collaborator: "Veerle Nanna",
